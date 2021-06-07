a91a670b0db8f5355f45325bf8faffe71f2fe4bd
pooja04work <work14206@gmail.com>
2021-06-08 01:08:01 +0530

    validate Name

--- javascript/payroll.js
+++ javascript/payroll.js
@@ -1,57 +1,56 @@
-alert("working");
-// //UC-9
-// class EmployeePayrollData {
+//UC-9
+class EmployeePayrollData {
 
-//     get id() { return this._id; }
-//     set id(id) {
-//         this._id = id;
-//     }
+    get id() { return this._id; }
+    set id(id) {
+        this._id = id;
+    }
 
-//     get name() { return this._name; }
-//     set name(name) {
-//         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-//         if (nameRegex.test(name)) {
-//             this._name = name;
-//         }
-//         else throw 'Name is Incorrect';
-//     }
+    get name() { return this._name; }
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (nameRegex.test(name)) {
+            this._name = name;
+        }
+        else throw 'Name is incorrect: ' + name;
+    }
 
-//     get profilepic() { return this._profilepic; }
-//     set profilepic(profilepic) {
-//         this._profilepic = profilepic;
-//     }
+    get profilepic() { return this._profilepic; }
+    set profilepic(profilepic) {
+        this._profilepic = profilepic;
+    }
 
-//     get gender() { return this._gender; }
-//     set gender(gender) {
-//         this._gender = gender;
-//     }
+    get gender() { return this._gender; }
+    set gender(gender) {
+        this._gender = gender;
+    }
 
-//     get department() { return this._department; }
-//     set department(department) {
-//         this._department = department;
-//     }
+    get department() { return this._department; }
+    set department(department) {
+        this._department = department;
+    }
 
-//     get salary() { return this._salary; }
-//     set salary(salary) {
-//         this._salary = salary;
-//     }
+    get salary() { return this._salary; }
+    set salary(salary) {
+        this._salary = salary;
+    }
 
-//     get note() { return this._note; }
-//     set note(note) {
-//         this._note = note;
-//     }
+    get note() { return this._note; }
+    set note(note) {
+        this._note = note;
+    }
 
-//     get startDate() { return this._startDate; }
-//     set startDate(startDate) {
-//         this._startDate = startDate;
-//     }
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
 
-//     toString() {
-//         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-//         const empDate = !this.startDate ? "undefined" :
-//             this.startDate.toLocaleeDateString("en-us", options);
-//         return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
-//             ", profilepic='" + this.profilepic + ", department='" + this.department +
-//             ", salary='" + this.salary + ", startDate='" + empDate + ", note='" + this.note;
-//     }
-// }
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" :
+            this.startDate.toLocaleeDateString("en-us", options);
+        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
+            ", profilepic='" + this.profilepic + ", department='" + this.department +
+            ", salary='" + this.salary + ", startDate='" + empDate + ", note='" + this.note;
+    }
+}
