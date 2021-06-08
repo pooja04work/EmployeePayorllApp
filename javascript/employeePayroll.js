79a6aa0c0be39e0060b15e28ba7722c7e36c1f35
pooja04work <work14206@gmail.com>
2021-06-09 00:06:50 +0530

    Added a file for reset button

--- javascript/employeePayroll.js
+++ javascript/employeePayroll.js
@@ -18,31 +18,37 @@ class EmployeePayrollData {
     get profilepic() { return this._profilepic; }
     set profilepic(profilepic) {
         this._profilepic = profilepic;
+        console.log(this._profilepic);
     }
 
     get gender() { return this._gender; }
     set gender(gender) {
         this._gender = gender;
+        console.log(this._gender);
     }
 
     get department() { return this._department; }
     set department(department) {
         this._department = department;
+        console.log(this._department);
     }
 
     get salary() { return this._salary; }
     set salary(salary) {
         this._salary = salary;
+        console.log(this._salary);
     }
 
     get note() { return this._note; }
     set note(note) {
         this._note = note;
+        console.log(this._note);
     }
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
         this._startDate = startDate;
+        console.log(this._startDate);
     }
 
     toString() {
