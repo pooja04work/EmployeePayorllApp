4ab5bfa0ec247f4e4bc9424e0139501b1973290e
pooja04work <work14206@gmail.com>
2021-06-09 02:24:06 +0530

    added a file for view EPD from JSON object

--- javascript/home.js
+++ javascript/home.js
@@ -5,23 +5,65 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHTML = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>startDate</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-    <tr>
-        <td>
-            <img src="../assets/images/Ellipse -1.png" alt="" class="profile">
-        </td>
-        <td>Liza</td>
-        <td>Female</td>
-        <td>
-            <div class="dept-label">Engineering</div>
-            <div class="dept-label">Finance</div>
-        </td>
-        <td>460000</td>
-        <td>1 dec 2019</td>
-        <td>
-            <img id="1" onclick="remove(this)" src="../assets/images/delete.png" alt="delete">
-            <img id="1" onclick="update(this)" src="../assets/images/edit.png" alt="edit">
-        </td>
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>
+                <img src="${empPayrollData._profilePic}" alt="" class="profile">
+            </td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/images/delete.png" alt="delete">
+                <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/images/edit.png" alt="edit">
+            </td>
         </tr>`;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+// UC5(day 47)
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Moitry',
+            _gender: 'Female',
+            _department: [
+                'Engineernig',
+                'HR'
+            ],
+            _salary: '460000',
+            _startDate: '2 dec 2019',
+            _note: 'Hello Moitry',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/images/Ellipse -4.png'
+        },
+        {
+            _name: 'kanika',
+            _gender: 'Female',
+            _department: [
+                'Engineernig'
+            ],
+            _salary: '550000',
+            _startDate: '1 jan 2020',
+            _note: 'Hello Kanika',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/images/Ellipse -7.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
